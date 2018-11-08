7309275def4bc73b76b1c8f038e954cd7bf96a69
dclcoconuts <dcl.corinnem@18pixel.fr>
2018-11-08 14:55:52 +0100

    Ajout du fichier de log

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- fichier modifié pour CSS par Maxime et Corinne -->
+<!-- Projet Table de multiplication -->
 <html lang="FR">
 <head>
     <meta charset="utf-8">
@@ -9,24 +9,38 @@
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> 
     <title>Lexique Balise</title>
 </head>
+
+<!-- mise en place d'un log d'erreur -->
+<?php
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__. '/.log_error');
+error_reporting(E_ALL & ~E_NOTICE);
+?>
+
+<!-- essai log d'erreur -->
+<?php 
+// error_log("Ecrit un message personnalisé dans le fichier log", 0);
+?> 
+
 <body>
     <header>
-        <!-- <?php include 'menu.php';?> -->
+
     </header>
         
     <main>
         <h1>Les tables de multiplication</h1>
             <form id="form" method="GET">
             <select id="select" name="tables">
-                <option id="choix" value="table02" <?php if($_GET['tables']=="table02") echo selected ?>>Table de 2</option>
-                <option id="choix" value="table03" <?php if($_GET['tables']=="table03") echo selected ?>>Table de 3</option>
-                <option id="choix" value="table04" <?php if($_GET['tables']=="table04") echo selected ?>>Table de 4</option>
-                <option id="choix" value="table05" <?php if($_GET['tables']=="table05") echo selected ?>>Table de 5</option>
-                <option id="choix" value="table06" <?php if($_GET['tables']=="table06") echo selected ?>>Table de 6</option>
-                <option id="choix" value="table07" <?php if($_GET['tables']=="table07") echo selected ?>>Table de 7</option>
-                <option id="choix" value="table08" <?php if($_GET['tables']=="table08") echo selected ?>>Table de 8</option>
-                <option id="choix" value="table09" <?php if($_GET['tables']=="table09") echo selected ?>>Table de 9</option>
-                <option id="choix" value="table10" <?php if($_GET['tables']=="table10") echo selected ?>>Table de 10</option>
+                <option id="choix" value="table02" <?php if($_GET['tables']=="table02") echo 'selected' ?>>Table de 2</option>
+                <option id="choix" value="table03" <?php if($_GET['tables']=="table03") echo 'selected' ?>>Table de 3</option>
+                <option id="choix" value="table04" <?php if($_GET['tables']=="table04") echo 'selected' ?>>Table de 4</option>
+                <option id="choix" value="table05" <?php if($_GET['tables']=="table05") echo 'selected' ?>>Table de 5</option>
+                <option id="choix" value="table06" <?php if($_GET['tables']=="table06") echo 'selected' ?>>Table de 6</option>
+                <option id="choix" value="table07" <?php if($_GET['tables']=="table07") echo 'selected' ?>>Table de 7</option>
+                <option id="choix" value="table08" <?php if($_GET['tables']=="table08") echo 'selected' ?>>Table de 8</option>
+                <option id="choix" value="table09" <?php if($_GET['tables']=="table09") echo 'selected' ?>>Table de 9</option>
+                <option id="choix" value="table10" <?php if($_GET['tables']=="table10") echo 'selected' ?>>Table de 10</option>
             </select> 
             <br><br>
             <input id="bouton" name="bouton" type="submit">
@@ -55,4 +69,4 @@
  
     </footer>
 </body>
- </html>
+</html>
